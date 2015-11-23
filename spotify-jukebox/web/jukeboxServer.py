baea1c4b3f837f2a6ea15e0e5117876b414bda0c
patengelbert <patengelbert@hotmail.co.uk>
2015-11-23 17:22:21 +0000

    Added missing files for db management

--- spotify-jukebox/web/jukeboxServer.py
+++ spotify-jukebox/web/jukeboxServer.py
@@ -1,61 +1,67 @@
 # import the Flask class from the flask module
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash, json
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash, json
 from flask.json import jsonify
-from flask.ext.login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
-from falsk.ext.sqlalchemy import SQLAlchemy
+
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, UserMixin, current_user, login_user, \
+    logout_user, login_required
+    
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+from passlib.hash import pbkdf2_sha256
+
 
 # create the application object
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.setup_app(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/[YOUR_DATABASE_NAME]'
+app.config.from_pyfile('jukebox.cfg')
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+if not database_exists(engine.url):
+    create_database(engine.url)
+    
 db = SQLAlchemy(app)
 
 
 class UserNotFoundError(Exception):
     pass
 
-class UserDB(object):
-    # proxy for a database of users
-    user_database = {'admin@admin': {'pw': 'admin',
-				    'first_name': 'Admin',
-				    'last_name': 'admin'}}
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    password = db.Column(db.String(120))
     
-    def add(self, user, pw):
-        self.user_database[user] = {'pw': pw}
-
-    def get(self, user):
-        return (user, self.user_database.get(user))
-
-    def update(self, user):
-        self.user_database[user] = {'pw': user.password,
-                                    'first_name':user.first_name,
-                                    'last_name':user.last_name}
-
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.first_name = None
+        self.last_name = None
+        self.password = password
+        
+    def is_authenticated(self):
+        return True
+ 
+    def is_active(self):
+        return True
+ 
+    def is_anonymous(self):
+        return False
+ 
+    def get_id(self):
+        return unicode(self.id)
+        
+    def __repr__(self):
+        return '<User %r>' % self.username
     
-user_database = UserDB()
+db.create_all()
 
-class User(UserMixin):
-
-    def __init__(self, id):
-        self.id, self.password = user_database.get(id)
-	self.first_name = self.password.get('first_name')
-	self.last_name = self.password.get('last_name')
-	self.password = self.password['pw']
-        if self.id is None or self.password is None:
-            raise UserNotFoundError()
-
-    @classmethod
-    def get(self_class, id):
-        '''Return user instance of id, return None if not exist'''
-        try:
-            return self_class(id)
-        except UserNotFoundError:
-            return None
-
-    def update(self):
-        user_database.update(self)
 
 @login_manager.unauthorized_handler
 def unauthorized():
@@ -64,7 +70,7 @@ def unauthorized():
 # Flask-Login use this to reload the user object from the user ID stored in the session
 @login_manager.user_loader
 def load_user(id):
-    return User.get(id)
+    return User.query.get(int(id))
 
 # use decorators to link the function to a url
 @app.route('/')
@@ -82,11 +88,11 @@ def register():
     error = None
     if request.method == 'POST':
         id = request.form['username']
-        pass1 = request.form['password']
-        pass2 = request.form['passwordrepeat']
-        if (pass1 == pass2):
-            user_database.add(id, pass1)
-            user = User.get(id)
+        password = pbkdf2_sha256.encrypt(request.form['password'], rounds=200000, salt_size=16)
+        if pbkdf2_sha256.verify(request.form['passwordrepeat'], password):
+            user = User(id, id, password)
+            db.session.add(user)
+            db.session.commit()
             login_user(user)
             return jsonify(), 200
         else:
@@ -100,10 +106,13 @@ def account():
     error = None
     if request.method == 'POST':
         user = current_user
-        user.first_name = request.form.get('firstname')
-        user.last_name = request.form.get('lastname')
-        user.password = request.form.get('password')
-        user.update()
+        if request.form.get('firstname') != None and request.form.get('firstname') != user.first_name:
+            user.first_name = request.form.get('firstname')
+        if request.form.get('lastname') != None and request.form.get('lastname') != user.last_name:
+            user.last_name = request.form.get('lastname')
+        if request.form.get('password') != None and request.form.get('password') != user.password:
+            user.password = request.form.get('password')
+        db.session.commit()
     return render_template('account.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -111,8 +120,8 @@ def login():
     error = None
     if request.method == 'POST':
         # retrieve username and password
-        user = User.get(request.form['username'])
-        if (user and user.password == request.form['password']):
+        user = User.query.filter_by(username=request.form['username']).first()
+        if (user and pbkdf2_sha256.verify(request.form['password'], user.password)):
             remember_me = request.form.get('remember') is not None
             login_user(user, remember=remember_me)
             return jsonify(), 200
@@ -130,10 +139,8 @@ def logout():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-app.secret_key = '"\x0fNG%\'w\xa30\xc1\83\xe7\xb9Ay\xbe\x0e\x13W\xc3\xd1d]\xba'
-
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
 
